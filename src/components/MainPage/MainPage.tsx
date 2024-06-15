ccd1122e491697e74f80006ed92bef1fc7b61c05
Guilherme Eger <guilhermeeger.developer@gmail.com>
2024-06-15 18:00:54 +1000

    FIrst commit, starting project

---
+++ src/components/MainPage/MainPage.tsx
@@ -0,0 +1,10 @@
+import { FC } from 'react';
+import './MainPage.css'
+
+type Props = {};
+const MainPage: FC<Props> = ({ }) => {
+    return <>
+        Main page!
+    </>;
+};
+export default MainPage;
